440c8be4ab8e8aec629a5208c22aba1c85ba463f
anaesse <sophiesse143@gmail.com>
2023-09-09 12:46:18 +0100

    made some changes to the data attributes

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-const UTC = new Date();
-let ms = UTC.getUTCMilliseconds();
-// UTC.setUTCMilliseconds(UTC.getUTCMilliseconds());
-document.getElementById("currentUTCTime").innerHTML = ms;
-
 const week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const presentDay = new Date();
 let Day = week[presentDay.getDay() ]
-document.getElementById("currentDay").innerHTML = Day
+document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = Day
+
+const UTC = new Date();
+let ms = UTC.getUTCMilliseconds();
+document.querySelector('[data-testid="currentUTCTime"]').textContent = ms;
+
+
+
+
